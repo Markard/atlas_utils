a0afaa729a531d4f7c269afdfdd9a0a77d39bd98
Dmitrii Iuzhakov <dmtry88@gmail.com>
2025-03-30 13:36:08 +0300

    Setup command to get all spaces

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import click
 import dotenv
 
 from export_pdf.cli_command import export_pdf
+from get_space_keys.cli_command import get_space_keys
 
 
 @click.group()
@@ -10,6 +11,7 @@ def entry_point():
 
 
 entry_point.add_command(export_pdf)
+entry_point.add_command(get_space_keys)
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
